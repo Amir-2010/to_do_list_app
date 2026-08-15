305401d75ca886b5530884aba36780a2ec7e5f46
Amir-2010 <Amir.hozhabri.rk2025@gmail.com>
2026-08-15 09:17:10 +0330

    add user_id column

--- app/tasks/models.py
+++ app/tasks/models.py
@@ -2,10 +2,25 @@
 # models is for making a table of data base instead create them in database.py
 from sqlalchemy import Column,String,Integer,Boolean
 from app.database import Base
+from sqlalchemy import ForeignKey
 
 class task_models(Base):
     __tablename__ = "tasks"
     id = Column(Integer,primary_key=True,autoincrement=True)
+    user_id = Column(Integer,ForeignKey("users.id"))
     title = Column(String,nullable=False)
     description = Column(String(500),nullable=True)
     work_status = Column(Boolean,default=False)
+
+    def __repr__(self):
+        return f"tasks(id={self.id},user id={self.user_id},title={self.title},description={self.description},work status={self.work_status})"
+
+class users_models(Base):
+    __tablename__ = "users"
+    id = Column(Integer,primary_key=True,autoincrement=True)
+    name = Column(String,nullable=False)
+    password = Column(String,nullable=False)
+    user_status = Column(Boolean,default=True)
+
+    def __repr__(self):
+        return f"users(id={self.id},name={self.name},password={self.password},user status={self.user_status})"
--- app/tasks/routes.py
+++ app/tasks/routes.py
@@ -1,5 +1,5 @@
 
-from fastapi import APIRouter,Depends,status,HTTPException
+from fastapi import APIRouter,Depends,status,HTTPException,Query
 from sqlalchemy.orm import Session
 from app.database import get_db
 from tasks.models import task_models
@@ -8,12 +8,12 @@ router = APIRouter()
 
 # in here if we get tags to the router the label change from default to tag we want
 # the rags should be in a list
-@router.get("/get all works",tags=["get all works"])
+@router.get("/get_all_works",tags=["get all works"])
 async def read_works(db:Session=Depends(get_db)):
     query = db.query(task_models)
     return query.all()
 
-@router.get("/find a work",tags=["get one work"],status_code=status.HTTP_200_OK)
+@router.get("/find_a_work",tags=["get one work"],status_code=status.HTTP_200_OK)
 async def get_work(work_title,db:Session=Depends(get_db)):
     query = db.query(task_models)
     result = query.where(task_models.title.like(f"%{work_title}%")).all()
@@ -22,20 +22,35 @@ async def get_work(work_title,db:Session=Depends(get_db)):
     else:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="work not found")
 
-@router.post("/add work",tags=["add work"])
+@router.get("/find_complete_or_incomplete_works",tags=["get complete or incomplete"])
+def get_complete_or_incomplete_works(
+    find_status:bool=Query(None),
+    db:Session=Depends(get_db),
+    limited:int|None=Query(default=None,ge=0,description="this is how many data you want to see"),
+    offset_var:int|None=Query(default=None,ge=0,description="this for how many data did you see")
+    ):
+    query = db.query(task_models)
+    if find_status != None:
+        result = query.where(task_models.work_status==find_status)
+        return result.limit(limited).offset(offset_var).all()
+    else:
+        return query.limit(limited).offset(offset_var).all()
+
+@router.post("/add_work",tags=["add work"])
 async def add_work(work_title:str,description:str|None=None,db:Session=Depends(get_db)):
     query = db.query(task_models)
     check_duplicate = query.where(task_models.title == work_title).one_or_none()
     if check_duplicate == None:
-        work_obj = task_models(title=work_title,description=description)
+        get_id = query.where(task_models.title==work_title).one_or_none()
+        work_obj = task_models(title=work_title,description=description,user_id=get_id)
         db.add(work_obj)
         db.commit()
         result = query.where(task_models.title == work_title).one_or_none()
         return result
     else:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="work not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="duplicate work")
 
-@router.put("/new title",tags=["update title"])
+@router.put("/new_title",tags=["update title"])
 async def update_work(work_title:str,new_title:str,db:Session=Depends(get_db)):
     query = db.query(task_models)
     find_work = query.where(task_models.title == work_title).one_or_none()
@@ -47,7 +62,7 @@ async def update_work(work_title:str,new_title:str,db:Session=Depends(get_db)):
     else:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="work not found")
 
-@router.put("/new description",tags=["update description"])
+@router.put("/new_description",tags=["update description"])
 async def change_description(work_title:str,new_description:str|None=None,db:Session=Depends(get_db)):
     query = db.query(task_models)
     find_work = query.where(task_models.title == work_title).one_or_none()
@@ -59,7 +74,7 @@ async def change_description(work_title:str,new_description:str|None=None,db:Ses
     else:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="work not found")
 
-@router.put("/new status",tags=["update status"])
+@router.put("/new_status",tags=["update status"])
 async def change_status(work_title:str,db:Session=Depends(get_db)):
     query = db.query(task_models)
     find_work = query.where(task_models.title == work_title).one_or_none()
@@ -71,7 +86,7 @@ async def change_status(work_title:str,db:Session=Depends(get_db)):
     else:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="work not found")
 
-@router.delete("/delete works",tags=["delete works"])
+@router.delete("/delete_works",tags=["delete works"])
 async def delete_work(work_title,db:Session=Depends(get_db)):
     query = db.query(task_models)
     find_work = query.where(task_models.title == work_title).one_or_none()
